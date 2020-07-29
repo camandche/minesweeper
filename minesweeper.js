e04590b1d28e1a26a2aa7f70ff59a4ed0e5042b4
Cheryl Wichman <camandche@gmail.com>
2020-07-29 15:17:59 +1200

    Fixed ReloadPage Error

--- minesweeper.js
+++ minesweeper.js
@@ -1,18 +1,21 @@
 document.addEventListener('DOMContentLoaded', startGame)
-document.addEventListener("click", checkForWin)
+document.addEventListener("click", checkForWin);
 document.addEventListener("contextmenu", checkForWin)
 
 // Define your `board` object here!
-var board = {
-  cells: [{row: 0, col: 0, isMine: false, isMarked: false, hidden: true}, {row: 0, col: 1, isMine: false, isMarked: false, hidden: true}, {row: 0, col: 2, isMine: false, isMarked: false, hidden: true}, {row: 0, col: 3, isMine: true, isMarked: false, hidden: true},
-          {row: 1, col: 0, isMine: true, isMarked: false, hidden: true}, {row: 1, col: 1, isMine: true, isMarked: false, hidden: true}, {row: 1, col: 2, isMine: false, isMarked: false, hidden: true}, {row: 1, col: 3, isMine: false, isMarked: false, hidden: true},
-          {row: 2, col: 0, isMine: false, isMarked: false, hidden: true}, {row: 2, col: 1, isMine: false, isMarked: false, hidden: true}, {row: 2, col: 2, isMine: false, isMarked: false, hidden: true}, {row: 2, col: 3, isMine: true, isMarked: false, hidden: true},
-          {row: 3, col: 0, isMine: true, isMarked: false, hidden: true}, {row: 3, col: 1, isMine: false, isMarked: false, hidden: true}, {row: 3, col: 2, isMine: true, isMarked: false, hidden: true}, {row: 3, col: 3, isMine: true, isMarked: false, hidden: true}]
-}
 //Display counts of how many surrounding cells contain mines
-  //Use global board to store info about gameboard
-  //Store how many mines surround each cell
-  //Count them & store globally
+//Use global board to store info about gameboard
+//Store how many mines surround each cell
+//Count them & store globally
+
+var board = {
+  cells: [{row: 0, col: 0, isMine: false, isMarked: false, hidden: true}, {row: 0, col: 1, isMine: false, isMarked: false, hidden: true}, {row: 0, col: 2, isMine: true, isMarked: false, hidden: true}, {row: 0, col: 3, isMine: false, isMarked: false, hidden: true}, {row: 0, col: 4, isMine: false, isMarked: false, hidden: true},  {row: 0, col: 5, isMine: false, isMarked: false, hidden: true},
+          {row: 1, col: 0, isMine: true, isMarked: false, hidden: true}, {row: 1, col: 1, isMine: true, isMarked: false, hidden: true}, {row: 1, col: 2, isMine: false, isMarked: false, hidden: true}, {row: 1, col: 3, isMine: false, isMarked: false, hidden: true},  {row: 1, col: 4, isMine: false, isMarked: false, hidden: true},  {row: 1, col: 5, isMine: true, isMarked: false, hidden: true},
+          {row: 2, col: 0, isMine: false, isMarked: false, hidden: true}, {row: 2, col: 1, isMine: false, isMarked: false, hidden: true}, {row: 2, col: 2, isMine: false, isMarked: false, hidden: true}, {row: 2, col: 3, isMine: true, isMarked: false, hidden: true}, {row: 2, col: 4, isMine: true, isMarked: false, hidden: true}, {row: 2, col: 5, isMine: true, isMarked: false, hidden: true},
+          {row: 3, col: 0, isMine: true, isMarked: false, hidden: true}, {row: 3, col: 1, isMine: false, isMarked: false, hidden: true}, {row: 3, col: 2, isMine: true, isMarked: false, hidden: true}, {row: 3, col: 3, isMine: false, isMarked: false, hidden: true}, {row: 3, col: 4, isMine: false, isMarked: false, hidden: true}, {row: 3, col: 5, isMine: false, isMarked: false, hidden: true},
+          {row: 4, col: 0, isMine: true, isMarked: false, hidden: true}, {row: 4, col: 1, isMine: true, isMarked: false, hidden: true}, {row: 4, col: 2, isMine: false, isMarked: false, hidden: true}, {row: 4, col: 3, isMine: false, isMarked: false, hidden: true}, {row: 4, col: 4, isMine: false, isMarked: false, hidden: true}, {row: 4, col: 5, isMine: true, isMarked: false, hidden: true},
+          {row: 5, col: 0, isMine: false, isMarked: false, hidden: true}, {row: 5, col: 1, isMine: false, isMarked: false, hidden: true}, {row: 5, col: 2, isMine: false, isMarked: false, hidden: true}, {row: 5, col: 3, isMine: true, isMarked: false, hidden: true}, {row: 5, col: 4, isMine: true, isMarked: false, hidden: true}, {row: 5, col: 5, isMine: false, isMarked: false, hidden: true},]
+}
   //Write a 'for' loop to loop through board.cells content
   //Loops to call 'countSurroundingMines' one for each cell in 'board.cells'
   //Parse each cell as an argument (...)
@@ -30,6 +33,7 @@ var board = {
 function startGame () {
   for (let i = 0; i < board.cells.length; i++) {
     board.cells[i].surroundingMines = countSurroundingMines(board.cells[i])
+    isMine: (Math.random() >= 0.5)
   }
 
   // Don't remove this function call: it makes the game work!
@@ -44,13 +48,17 @@ function checkForWin () {
   var win = true;
   for (let i = 0; i < board.cells.length; i++) {
     if ((board.cells[i].isMine === true && board.cells[i].isMarked === false) || (board.cells[i].isMine === false && board.cells[i].isMarked === true))  {
-    win = false; }
+    win = false;
+      var randomIndex = Math.floor(Math.random () *cells.length)
+      var randomCell = cells[randomIndex]
+    }
   }
 
   // You can use this function call to declare a winner (once you've
   // detected that they've won, that is!)
     if (win == true) {
       lib.displayMessage('You win!')
+      empty();
     }
 }
 
@@ -69,4 +77,19 @@ function countSurroundingMines (cell) {
       }
     }
   return count;
+}
+// // var resetBoard = 0
+// function resetBoard() {
+//   console.log(resetBoard);
+//   document.querySelector(startGame) 
+//     board = {},
+//     lib.initBoard()
+//     clearAll();
+//   };
+// resetBoard();
+
+// var refresh = document.getElementById("reload").onclick = reloadPage;
+
+function reloadPage() {
+  window.location.reload();
 }
